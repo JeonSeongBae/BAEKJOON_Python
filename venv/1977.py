a04e76176bd4fc9c9e3521b367ff2912d76de20d
MaengMaeng <maybe8189@naver.com>
2018-04-26 16:37:56 +0900

    re solve algo 1977, O(n) >> O(n ** 0.5)

--- venv/1977.py
+++ venv/1977.py
@@ -6,17 +6,21 @@
 # 출력 : M이상 N이하의 자연수 중 완전제곱수인 것을 모두 찾아 첫째 줄에 그 합을, 둘째 줄에 그 중 최소값을 출력한다.
 # 단, M이상 N이하의 자연수 중 완전제곱수가 없을 경우는 첫째 줄에 -1을 출력한다.
 import math
+
+# for i in range(M, N+1):
+#     temp = int(math.pow(int(math.sqrt(i)), 2))
+#     if temp == i:
+#         num.append(i)
 M = int(input())
 N = int(input())
 
 num = []
 
+if int(int(M ** 0.5) ** 2) == M:
+    num.append(M)
 
-for i in range(M, N+1):
-    temp = int(math.pow(int(math.sqrt(i)), 2))
-    if temp == i:
-        num.append(i)
-
+for i in range(int(M ** 0.5) + 1, int(N ** 0.5) + 1):
+    num.append(int(i ** 2))
 
 if len(num) is 0:
     print(-1)

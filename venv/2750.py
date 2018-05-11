c7803d6ef19a37d08fe7ea51331857bd86160c04
JeonSeongBae <tjdqo6042@naver.com>
2018-05-11 19:25:14 +0900

    Bubble Sort

--- venv/2750.py
+++ venv/2750.py
@@ -1,12 +1,22 @@
 # -*- Encoding:UTF-8 -*-
 
-# 백준 알고리즘 1475번
+# 백준 알고리즘 2750번
 #
-# 다솜이는 은진이의 옆집에 새로 이사왔다. 다솜이는 자기 방 번호를 예쁜 플라스틱 숫자로 문에 붙이려고 한다.
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
 #
-# 다솜이의 옆집에서는 플라스틱 숫자를 한 세트로 판다. 한 세트에는 0번부터 9번까지 숫자가 하나씩 들어있다. 다솜이의 방 번호가 주어졌을 때, 필요한 세트의 개수의 최소값을 출력하시오. (6은 9를 뒤집어서 이용할 수 있고, 9는 6을 뒤집어서 이용할 수 있다.)
 
-roomNumber = input()
+size = int(input())
+array = []
 
-array = [0 for i in range(0, 8)]
+for i in range(0, size):
+    array.append(int(input()))
 
+for i in range(0, size):
+    for j in range(i, size):
+        if(array[i] > array[j]):
+            temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+
+for i in range(0, size):
+    print(array[i])

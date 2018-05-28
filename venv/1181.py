3fdf974e7892ea4493a115adcebf4c3387ef8d1b
JeonSeongBae <tjdqo6042@naver.com>
2018-05-28 16:54:52 +0900

    1181_sorting list size and alpha

---
+++ venv/1181.py
@@ -0,0 +1,14 @@
+# -*- Encoding:UTF-8 -*-
+
+# 백준 알고리즘 1181번
+
+inputString = []
+size = int(input())
+
+for i in range(size):
+    inputString.append(input())
+
+def strange_sort(strings):
+    return sorted(strings, key=lambda strings:(len(strings), strings))
+
+print(strange_sort(inputString))

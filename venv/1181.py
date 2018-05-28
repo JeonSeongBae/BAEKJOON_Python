d5f03ae4abdcbd4e7e36ec13ea3671147f9195d2
JeonSeongBae <tjdqo6042@naver.com>
2018-05-28 16:59:57 +0900

    1181_print list

--- venv/1181.py
+++ venv/1181.py
@@ -12,4 +12,8 @@ def strange_sort(strings):
     return sorted(strings, key=lambda strings:(len(strings), strings))
 
 inputString = list(set(inputString))
-print(strange_sort(inputString))
+
+inputString = strange_sort(inputString)
+
+for i in inputString:
+    print(i)
